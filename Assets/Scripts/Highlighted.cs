cfe1e408a25360eedd8cc96d087d8a0763596ad9
Gabriel Loçasso <33566334+Locasso@users.noreply.github.com>
2021-05-22 20:18:28 -0300

    Re adding some sprites

---
+++ Assets/Scripts/Highlighted.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Highlighted : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler
+{
+     [SerializeField] private Animator xBtnAnim;
+
+     public void OnPointerEnter(PointerEventData eventData)
+     {
+         xBtnAnim.SetTrigger("Highlighted");
+     }
+     
+     public void OnPointerExit(PointerEventData eventData)
+     {
+          
+     }
+}
